fa11d056c1ded4bec2fb17dbc9ec021f7f1ebbdc
Harry Groover <hjgetc@rit.edu>
2016-08-12 19:09:12 -0400

    add anthem of hope page, update menu

--- views/anthem_of_hope.php
+++ views/anthem_of_hope.php
@@ -1,8 +1,48 @@
 
 <?php require_once(BASE_PATH.'views/templates/header.php'); ?>
 
-<h1>
+<h2 class="page-title">
 	Anthem of Hope
-</h1>
+</h2>
+
+<div class="minor-info spacer-bottom-large">
+	Click play on the song above to play the song.
+</div>
+
+<p>
+	Someday, Somewhere<br/>
+	We'll find a CURE and start living,<br/>
+	We'll find a new way of fighting<br/>
+	Somehow.
+</p>
+<p>
+	There's HOPE for us,<br/>
+	Someday a CURE will come<br/>
+	No more needles and No more pain<br/>
+	Waits for us somewhere.
+</p>
+<p>
+	Now's the time for us<br/>
+	THE CURE is coming soon<br/>
+	Time for research &amp; funding too<br/>
+	Time to look, Time to care.
+</p>
+<p>
+	Someday, Someway-<br/>
+	We'll find THE CURE and start living<br/>
+	We'll find it soon- Keep believing<br/>
+	Someday, Someway<br/>
+	There's a CURE out there<br/>
+	I know we'll find a CURE someday<br/>
+	Hold my hand cuz we're half way there<br/>
+	Hold my hand and say a prayer
+</p>
+<p>
+	Somewhere Somehow Someday<br/>
+	Adapted Lyrics By<br/>
+	Molly Johnson -2005<br/>
+	Based on "Somewhere There's a Place for Us" from West Side Story. Original Lyrics by Stephen Sondheim &amp; Original Music by Leonard Bernstein<br/>
+	Track Obtained through Master Use License from Priddis.com
+</p>
 
 <?php require_once(BASE_PATH.'views/templates/footer.php'); ?>
--- views/templates/menu.php
+++ views/templates/menu.php
@@ -9,8 +9,8 @@
 				$selected = 1;
 				break;
 
-			case 'our_favorite_charities.php':
-				$selected = 3;
+			case 'anthem_of_hope.php':
+				$selected = 2;
 				break;
 
 			case 'events_and_links.php':
@@ -19,10 +19,6 @@
 
 			case 'thank_you.php':
 				$selected = 5;
-				break;
-
-			case 'contact.php':
-				$selected = 6;
 				break;
 		}
 	}
@@ -35,16 +31,13 @@
 	<li class="<?php if ($selected == 1) echo 'selected' ?>">
 		<a href="<?php echo BASE_URL; ?>our_story" alt="Our Story"><span>Our Story</span></a>
 	</li>
-	<li class="<?php if ($selected == 3) echo 'selected' ?>">
-		<a href="<?php echo BASE_URL; ?>our_favorite_charities" alt="Charities"><span>Charities</span></a>
+	<li class="<?php if ($selected == 2) echo 'selected' ?>">
+		<a href="<?php echo BASE_URL; ?>anthem_of_hope" alt="Anthem of Hope"><span>Anthem of Hope</span></a>
 	</li>
 	<li class="<?php if ($selected == 4) echo 'selected' ?>">
 		<a href="<?php echo BASE_URL; ?>events_and_links" alt="Events and Links"><span>Events &amp; Links</span></a>
 	</li>
 	<li class="<?php if ($selected == 5) echo 'selected' ?>">
 		<a href="<?php echo BASE_URL; ?>thank_you" alt="Thank You"><span>Thank You</span></a>
-	</li>
-	<li class="<?php if ($selected == 6) echo 'selected' ?>">
-		<a href="<?php echo BASE_URL; ?>contact" alt="Contact Us"><span>Contact Us</span></a>
 	</li>
 </ul>
