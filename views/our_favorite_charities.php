94d5ee4e92ef3ee0b69e5abedf1c81f4bf2881b5
Harry Groover <hjgetc@rit.edu>
2016-08-15 08:54:39 -0400

    update charities, for css refresh

--- views/our_favorite_charities.php
+++ views/our_favorite_charities.php
@@ -1,8 +1,103 @@
 
 <?php require_once(BASE_PATH.'views/templates/header.php'); ?>
 
-<h2 class="page-title">
-	Our Favorite Charities
-</h2>
+<div class="page-with-sidebar-right">
+
+	<div class="sidebar-right">
+		<h3 class="spacer-bottom-larger">
+			Sarah's Favorite Artists
+		</h3>
+
+		<p>
+			<a href="/web/20131208122625/http://www.billygilman.com/online/billygilman_riiff05_photos/billygilman_riiff_autographs/billy_riiff_auto01.html" title="click here" target="_blank">Billy Gilman</a>
+		</p>
+
+		<p><a href="/web/20131208122625/http://youtu.be/vEGBmXXYJxg" title="Cassandra Kubinski" target="_blank">Cassandra Kubinski</a></p>
+
+		<p><a href="/web/20131208122625/http://www.jonasbrothers.com/" target="_blank">The Jonas Brothers</a></p>
+
+		<p><a href="/web/20131208122625/http://www.justinbiebermusic.com/" target="_blank">Justin Bieber</a></p>
+
+		<p><a href="/web/20131208122625/http://www.mileycyrus.com/" target="_blank">Miley Cyrus</a></p>
+
+		<p><a href="/web/20131208122625/http://www.demilovato.com/" target="_blank">Demi Lovato</a></p>
+
+		<p><a href="/web/20131208122625/http://selenagomez.com/" target="_blank">Selena Gomez</a></p>
+
+        <p><a href="/web/20131208122625/http://onedirection.com/" target="_blank">One Direction</a></p>
+
+        <p><a href="/web/20131208122625/http://taylorswift.com/" target="_blank">Taylor Swift</a></p>
+
+        <p><a href="/web/20131208122625/http://edsheeran.com/" target="_blank">Ed Sheeran</a></p>
+
+        <p><a href="/web/20131208122625/http://officialbirdy.com/" target="_blank">Birdy</a></p>
+
+        <p><a href="/web/20131208122625/http://timefliesmusic.com/" target="_blank">Timeflies</a></p>
+
+	</div>
+	<div class="left-content">
+		<h2 class="page-title">
+			Our Favorite Charities
+		</h2>
+
+		<p>
+			<a href="http://www.jdrf.org/" target="_blank" title="www.jdrf.org">Juvenile Diabetes Research Foundation</a>
+		</p>
+		<p>
+			Over the past six years, JDRF has consistently been ranked among the top charities in the U.S. by third-party watchdogs such as the Chronicle of Philanthropy and Forbes magazine.
+		</p>
+		<p>
+			In a recent Forbes article JDRF was profiled and praised for its exemplary focus and efficiency. "Send money to these people and you can be sure that only a small portion will be frittered away soliciting you for more, and the bulk of what's left will be invested in a single-minded drive to find a definitive cure for juvenile diabetes.
+		</p>
+
+		<p><a href="http://www.stjude.org/" title="www.stjude.org">St. Jude Children's Research Hospital</a></p>
+
+		<p><a href="http://www.huntershope.org/">Hunters Hope</a></p>
+
+		<p><a href="http://myeloma.org/">International Myeloma Foundation</a></p>
+
+		<p><a href="http://www.cancer.org/" title="www.cancer.org">American Cancer Society</a></p>
+
+		<p><a href="http://www.mda.org/" title="www.mda.org">Muscular Dystrophy Association</a></p>
+
+		<p><a href="http://www.cff.org/" title="www.cff.org">Cystic Fibrosis Foundation</a></p>
+
+		<p><a href="http://www.paralysis.org/" title="www.paralysis.org">Christopher Reeve Foundation</a></p>
+
+		<p><a href="http://www.nationalmssociety.org/" title="www.nationalmssociety.org">The National Multiple Sclerosis Society</a></p>
+
+		<p><a href="http://www.alz.org/" title="www.alz.org">Alzheimer's</a></p>
+
+		<p><a href="http://www.diabetes.org/" title="www.diabetes.org">American Diabetes Association</a></p>
+
+		<p><a href="http://www.pdf.org/" title="www.pdf.org">Parkinson's Foundation</a></p>
+
+		<p><a href="http://www.leukemia-lymphoma.org/all_page?item_id=22220" title="Leukemia &amp; Lymphoma Society">Leukemia &amp; Lymphoma Society</a></p>
+
+		<p>
+			The Leukemia &amp; Lymphoma Society link is dedicated to Granny and Mrs. Greco
+		</p>
+
+		<p>
+			<a href="http://www.brentshapiro.org/" title="Brent Shapiro Foundation for Awareness">The Brent Shapiro Foundation For Drug Awareness</a>
+		</p>
+
+		<p>
+			As we learned unfortunately- Addiction&nbsp; is also a very strong and deadly disease that we would also like to bring awareness to. It is too late for our son Michael however with awareness and enlightment into this disease, maybe we can save other families the pain that we had to deal with.&nbsp; This is dedicated to our Guardian Angel- Michael Alan Johnson (1979-1997)&nbsp; We love and miss him everyday... "<a href="1%20month.doc" title="1 month">1 month</a>"&nbsp; "<a href="2%20years.doc" title="2 years">2 years</a>"
+		</p>
+
+		<p><a href="http://www.magicjohnson.org/" title="http://www.magicjohnson.org">The Magic Johnson Foundation</a></p>
+
+		<p>
+			The Magic Johnson Foundation is dedicated to Rich Arnold
+		</p>
+
+		<p>
+			Rich Arnold was a very talented Songwriter and Artist who while after suffering a heart attack in 1991 and while recuperating in the hospital was inspired to write this song about Magic Johnson after watching TV coverage of Magic's announcement that he was HIV positive.&nbsp; Unfortunately Rich was taken from us way too soon by another heart attack in 1993.&nbsp; However his music lives on.&nbsp; To hear a copy of Rich's song " A Little More Magic"&nbsp; please click on the link <a href="http://www.dynrec.com/rich1.html" title="www.dynrec.com/rich1.html " target="_blank">www.dynrec.com/rich1.html</a>
+		</p>
+	</div>
+
+
+</div>
 
 <?php require_once(BASE_PATH.'views/templates/footer.php'); ?>
